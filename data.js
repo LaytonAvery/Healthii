14ac5b195bd22d42bd70fb0091f0aaf205755c65
LaytonAvery <laytonavery5@gmail.com>
2024-02-23 03:15:46 -0500

    added styling to form

---
+++ data.js
@@ -0,0 +1,9 @@
+const CATEGORIES = [
+	{ name: "technology", color: "#98C1D9" },
+	{ name: "science", color: "#533A7B" },
+	{ name: "fitness", color: "#9B5094" },
+	{ name: "news", color: "#4B244A" },
+	{ name: "family", color: "#9A94BC" },
+	{ name: "history", color: "#541A27" },
+	{ name: "global", color: "#8b5cf6" },
+];
